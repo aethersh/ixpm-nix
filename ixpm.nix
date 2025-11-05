2be4d3295a15f54436bb8b320e63c86c19a0dfd9
Henrik VT <commits@henrikvt.com>
2025-11-04 20:07:46 -0500

    pretty chill...

--- ixpm.nix
+++ ixpm.nix
@@ -3,14 +3,16 @@
   pkgs,
   php84,
   lib,
+  dataDir ? "/var/lib/ixp-manager",
+  logDir ? "/var/log/ixp-manager",
   ...
 }: let
   phpPackage = php84.withExtensions ({
-    enabled,
-    all,
-  }:
+      enabled,
+      all,
+    }:
     # xml extension is handled by composer
-    enabled ++ (with all; [memcached snmp intl curl rrd mbstring gd bcmath zip yaml memcache ds]));
+      enabled ++ (with all; [memcached snmp intl curl rrd mbstring gd bcmath zip yaml memcache ds]));
 in
   phpPackage.buildComposerProject2 rec {
     pname = "ixp-manager";
@@ -24,7 +26,7 @@ in
     };
 
     vendorHash = "sha256-TlJ9qFbOGypOpX4VY9Rt1/aaIX9eYwPjGJ6MeRQODnI=";
-    
+
     php = phpPackage;
 
     buildInputs = with pkgs; [
@@ -42,4 +44,32 @@ in
       apacheHttpdPackages.php
       perl540Packages.NetAddrIP
     ];
+
+    postInstall = ''
+      chmod -R u+w $out/share
+      mv $out/share/php/ixp-manager/* $out
+      rm -r $out/share
+
+      #rm -rf $out/logs $out/rrd $out/bootstrap/cache $out/storage $out/.env
+      #ln -s ${logDir} $out/logs
+      #ln -s ${dataDir}/config.php $out/config.php
+      #ln -s ${dataDir}/.env $out/.env
+      #ln -s ${dataDir}/rrd $out/rrd
+      #ln -s ${dataDir}/storage $out/storage
+      #ln -s ${dataDir}/cache $out/bootstrap/cache
+      
+      #ls -l $out
+      
+      php $out/artisan vendor:publish --provider="Fideloper\Proxy\TrustedProxyServiceProvider"
+    '';
+
+    passthru = {
+      phpPackage = phpPackage;
+    };
+
+    meta = with lib; {
+      description = "IXP Manager";
+      platforms = platforms.linux;
+      license = licenses.gpl3Only;
+    };
   }
